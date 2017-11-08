e3aaa52284a9c870b3db16b55f9f3633a46e3825
Kazi Fozle Azim Rabi <fozleazimrabi2@gmail.com>
2017-11-09 01:29:01 +0600

    change line chart to bar chart and add an avg line

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Sparklines, SparklinesLine} from 'react-sparklines';
+import {Sparklines, SparklinesBars, SparklinesReferenceLine} from 'react-sparklines';
 
 export default class Chart extends React.Component {
 
@@ -11,8 +11,9 @@ export default class Chart extends React.Component {
     return (
       <div>
       	<Sparklines width={180} height={120} data={this.props.data}>
-      		<SparklinesLine color={this.props.color} />
-      	</Sparklines>
+      		<SparklinesBars style={{ fill: this.props.color }} />
+      	  <SparklinesReferenceLine type="avg" />
+        </Sparklines>
       </div>
     );
   }
